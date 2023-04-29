5fcc02328da1badd2b35dd9dac9d799fa6cefdab
2jin8 <78083320+2jin8@users.noreply.github.com>
2023-04-30 00:46:58 +0900

    알림장 수정, 삭제 추가

--- allimi/src/main/java/kr/ac/kumoh/allimi/repository/NoticeRepository.java
+++ allimi/src/main/java/kr/ac/kumoh/allimi/repository/NoticeRepository.java
@@ -2,6 +2,7 @@ package kr.ac.kumoh.allimi.repository;
 
 import kr.ac.kumoh.allimi.domain.Facility;
 import kr.ac.kumoh.allimi.domain.NHResident;
+import kr.ac.kumoh.allimi.domain.User;
 import kr.ac.kumoh.allimi.domain.func.Notice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -11,16 +12,14 @@ import java.util.Optional;
 
 public interface NoticeRepository extends JpaRepository<Notice, Long> {
 
-//    @Query("select ntc from Notice ntc where ntc.facility = ?1 order by ntc.createDate desc")
-//    Optional<List> findAllByFacility(Facility facility);
-//
-//    @Query("select ntc from Notice ntc where ntc.nhResident = ?1 order by ntc.createDate desc")
-//    Optional<List> findAllByTarget(NHResident target);
-//
-//    Optional<Notice> findById(Long noticeId);
-//
-//    Long deleteNoticeById(Long noticeId);
-//
-//    void deleteByIdIn(List<Long> ids);
-//    Optional<List> findByUserOrTarget(User user, NHResident target);
+    @Query("select ntc from Notice ntc where ntc.facility = ?1 order by ntc.createDate desc")
+    Optional<List> findAllByFacility(Facility facility);
+
+    @Query("select ntc from Notice ntc where ntc.nhResident = ?1 order by ntc.createDate desc")
+    Optional<List> findAllByTarget(NHResident target);
+
+    Optional<Notice> findNoticeByNoticeId(Long noticeId);
+
+    Long deleteNoticeByNoticeId(Long noticeId);
+
 }
