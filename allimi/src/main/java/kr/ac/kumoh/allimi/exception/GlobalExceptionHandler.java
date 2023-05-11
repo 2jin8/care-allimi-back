7841850d2790e1abb0d9f00fff86c8801a41b1b1
2jin8 <78083320+2jin8@users.noreply.github.com>
2023-05-11 14:15:44 +0900

    AllNoticeService write null 확인 추가

---
+++ allimi/src/main/java/kr/ac/kumoh/allimi/exception/GlobalExceptionHandler.java
@@ -0,0 +1,81 @@
+package kr.ac.kumoh.allimi.exception;
+
+import kr.ac.kumoh.allimi.exception.user.UserAuthException;
+import kr.ac.kumoh.allimi.exception.user.UserException;
+import kr.ac.kumoh.allimi.exception.user.UserIdDuplicateException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler({UserException.class})
+    protected ResponseEntity handleUserException(UserException e) {
+        log.info("UserException = {}", e.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
+
+    @ExceptionHandler({UserIdDuplicateException.class})
+    protected ResponseEntity handleUserDuplicateException(UserIdDuplicateException e) {
+        log.info("UserIdDuplicateException = {}", e.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
+
+    @ExceptionHandler({UserAuthException.class})
+    protected ResponseEntity handleUserAuthException(UserAuthException e) {
+        log.info("UserAuthException = {}", e.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
+
+    @ExceptionHandler({FacilityException.class})
+    protected ResponseEntity handleFacilityException(FacilityException e) {
+        log.info("FacilityException={}", e);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
+
+    @ExceptionHandler({AllNoticeException.class})
+    protected ResponseEntity handleAllNoticeException(AllNoticeException e) {
+        log.info("AllNoticeException={}", e);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
+
+    @ExceptionHandler({InvitationException.class})
+    protected ResponseEntity handleInvitationException(InvitationException e) {
+        log.info("InvitationException={}", e);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
+
+    @ExceptionHandler({LetterException.class})
+    protected ResponseEntity handleLetterException(LetterException e) {
+        log.info("LetterException={}", e);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
+
+    @ExceptionHandler({NHResidentException.class})
+    protected ResponseEntity handleNHResidentException(NHResidentException e) {
+        log.info("NHResidentException={}", e);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
+
+    @ExceptionHandler({NoticeException.class})
+    protected ResponseEntity handleNoticeException(NoticeException e) {
+        log.info("NoticeException={}", e);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
+
+    @ExceptionHandler({ScheduleException.class})
+    protected ResponseEntity handleScheduleException(ScheduleException e) {
+        log.info("ScheduleException={}", e);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
+
+    @ExceptionHandler({VisitException.class})
+    protected ResponseEntity handleVisitException(VisitException e) {
+        log.info("VisitException={}", e);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
+}
